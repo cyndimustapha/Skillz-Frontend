7d06d0838ed5e8da54399a44e4d3b0614c8aab05
cyndi mustapha <marrencyndi101@gmail.com>
2024-08-09 15:54:24 +0300

    creating sidebar and styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaHome,FaTachometerAlt, FaEnvelope, FaBell, FaSearch, FaCog, FaSignOutAlt } from 'react-icons/fa';
 import './Sidebar.css';
 
 const Sidebar = () => {
@@ -18,12 +18,50 @@ const Sidebar = () => {
             </div>
             <ul className="sidebar-menu">
                 <li>
-                    <Link to="/LearnersDashboard">Dashboard</Link>
+                    <Link to="/Home">
+                        <FaHome className="icon" /> Home
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/MessagesPage">Messages</Link>
+                    <Link to="/LearnersDashboard">
+                        <FaTachometerAlt className="icon" /> Dashboard
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/MessagesPage">
+                        <FaEnvelope className="icon" /> Chats
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Notifications">
+                        <FaBell className="icon" /> Notifications
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Browser">
+                        <FaSearch className="icon" /> Browse
+                    </Link>
                 </li>
             </ul>
+            <div className="sidebar-bottom">
+                <ul className="sidebar-menu">
+                    <li>
+                        <Link to="/Settings">
+                            <FaCog className="icon" /> Settings
+                        </Link>
+                    </li>
+                    <li>
+                        <button className="logout-button" onClick={() => {
+                            // Handle logout logic
+                            localStorage.removeItem('token');
+                            localStorage.removeItem('user');
+                            window.location.href = '/login'; // Redirect to login page
+                        }}>
+                            <FaSignOutAlt className="icon" /> Logout
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
