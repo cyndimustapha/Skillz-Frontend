b93aaf56bda1a13b29ec02445fd4d53ec5c805dc
Temba Sydney Gabriel <tembasydney@gmail.com>
2024-08-15 23:28:42 +0300

    payment working

--- src/components/MpesaPaymentForm.jsx
+++ src/components/MpesaPaymentForm.jsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 const MpesaPaymentForm = ({ loading, handlePhoneSubmit }) => {
   const [phoneNumber, setPhoneNumber] = useState("");
 
+  // Handle the form submission
+  const handleSubmit = () => {
+    // Call the parent function with the phoneNumber
+    handlePhoneSubmit(phoneNumber);
+  };
+
   return (
     <div className="p-6 bg-white rounded-lg shadow-lg w-full max-w-md mx-auto">
       <h2 className="text-2xl font-semibold mb-4">MPESA Payment</h2>
@@ -14,7 +20,7 @@ const MpesaPaymentForm = ({ loading, handlePhoneSubmit }) => {
         placeholder="07123..."
       />
       <button
-        onClick={handlePhoneSubmit}
+        onClick={handleSubmit} // Call handleSubmit which passes phoneNumber
         disabled={phoneNumber.length < 10 || loading}
         className={`w-full py-3 rounded-md text-white ${
           phoneNumber.length < 10 || loading ? "bg-blue-300" : "bg-blue-500"
